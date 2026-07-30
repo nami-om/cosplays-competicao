f70cadade4ccf01659f49869cfcbd794655d356b
nami-om <mariomheidemannvaz@gmail.com>
2026-07-29 21:59:10 -0300

    final

--- script.js
+++ script.js
@@ -1,6 +1,73 @@
 let participantes = []
 let participantesNegados = []
 
+
+
+function testar() {
+
+    // localStorage.setItem('teste', 20)
+    // let testeDeLeitura = localStorage.getItem('teste')
+    // console.log(testeDeLeitura)
+
+    carregarDados()
+    participantes = [
+
+        {
+            nome_part: "Ana Luíza Silva",
+            nome_perso: "Hatsune Miku",
+            midia: "Vocaloid",
+            prop: "Microfone com pedestal",
+            modalidade: "Desfile",
+            canon: true,
+            aceito: true
+        },
+        {
+            nome_part: "Carlos Eduardo Ramos",
+            nome_perso: "Geralt of Rivia",
+            midia: "The Witcher 3",
+            prop: "Espada de aço",
+            modalidade: "Interpretativo",
+            canon: true,
+            aceito: true
+        },
+        {
+            nome_part: "Beatriz Mendes",
+            nome_perso: "Jinx",
+            midia: "Arcane",
+            prop: "Metralhadora Pow-Pow",
+            modalidade: "Desfile",
+            canon: true,
+            aceito: true
+        },
+        {
+            nome_part: "Lucas Gabriel Santos",
+            nome_perso: "Tanjiro Kamado",
+            midia: "Demon Slayer",
+            prop: "Espada Nichirin",
+            modalidade: "Interpretativo",
+            canon: false,
+            aceito: false
+        },
+        {
+            nome_part: "Mariana Costa",
+            nome_perso: "Zelda",
+            midia: "The Legend of Zelda: Tears of the Kingdom",
+            prop: "Cetro Real e Escudo Hylian",
+            modalidade: "Desfile",
+            canon: false,
+            aceito: false
+        }
+    ]
+
+    console.log(participantes)
+    salvarDados()
+}
+
+
+
+
+
+
 function mudarTela() {
     const telas = document.querySelectorAll(".tela")
     telas.forEach(function (tela) {
@@ -18,6 +85,7 @@ function mostrarPart() {
 
 
 function finalizarInscri() {
+    carregarDados()
     const novoPart = {
         id: Date.now(), //conta os milisegundos desde 1980 (vai ser necessário mais pra frente)
         nome_part: document.getElementById('input-nome_part').value,
@@ -26,15 +94,16 @@ function finalizarInscri() {
         prop: document.getElementById('input-prop').value,
         modalidade: document.getElementById('input-modalidade').value,
         canon: document.getElementById('input-canon').checked,
+        aceito: false
     }
     if (novoPart.canon == true) {
-        participantes.push(novoPart)
-    } else {
-        participantesNegados.push(novoPart)
+        novoPart.aceito = true
     }
+    participantes.push(novoPart)
     console.log(participantes);
     console.log(participantesNegados);
 
+    salvarDados()
     limparFormulario()
 }
 
@@ -50,38 +119,103 @@ function limparFormulario() {
 
 
 
+let editando = -1
+
+function editarCad(i) {
+    editando = i
+    carregarDados()
+    document.getElementById('input-nome_part').value = participantes[i].nome_part;
+    document.getElementById('input-nome_perso').value = participantes[i].nome_perso;
+    document.getElementById('input-midia').value = participantes[i].midia;
+    document.getElementById('input-prop').value = participantes[i].prop;
+    document.getElementById('input-modalidade').value = participantes[i].modalidade;
+    document.getElementById('input-canon').value = participantes[i].canon;
+
+    document.getElementById("telaInscri").classList.add("ativa")
+
+    // novoPart.splice(i, 1); 
+}
+
+function salvarEditado() {
+    console.log(editando)
+    console.log(participantes[editando])
+    carregarDados()
+    participantes[editando].nome_part = document.getElementById('input-nome_part').value
+    participantes[editando].nome_perso = document.getElementById('input-nome_perso').value
+    participantes[editando].midia = document.getElementById('input-midia').value
+    participantes[editando].prop = document.getElementById('input-prop').value
+    participantes[editando].modalidade = document.getElementById('input-modalidade').value
+    participantes[editando].canon = document.getElementById('input-canon').value
+    salvarDados()
+}
+
+function excluirCad(i) {
+    carregarDados()
+    participantes.splice(i, 1);
+    salvarDados()
+}
+
+
+function salvarDados() {
+    localStorage.setItem('participantes', JSON.stringify(participantes))
+
+    // let texto = JSON.stringify(dinos)
+    // localStorage.setItem('dinos', texto)
+}
+
+function carregarDados() {
+    participantes = JSON.parse(localStorage.getItem('participantes')) || []
+}
+
+
+
 //mostrar participantes aceitos
 //mostrar participantes negados - não canons
 //como separar os aceitos dos negados?
 
 function partTotais() {
+    carregarDados()
+    console.log(participantes);
     document.getElementById('painel-participantes-negados').innerHTML = ''
     document.getElementById('painel-participantes-aceitos').innerHTML = ''
 
     for (let i = 0; i < participantes.length; i++) {
-        document.getElementById('painel-participantes-aceitos').innerHTML +=
-            `<div class="card-part">
-           <h2>${participantes[i].nome_part}</h2>
-           <p>Nome do personagem: ${participantes[i].nome_perso}</p>
-           <p>Mídia: ${participantes[i].midia}</p>
-           <p>Prop: ${participantes[i].prop}</p>
-           <p>Modalidade desejada: ${participantes[i].modalidade}</p>
-           <p>Canonico: ${participantes[i].canon}</p>
-        </div>
-        `
+        if (participantes[i].aceito == true) {
+
+            document.getElementById('painel-participantes-aceitos').innerHTML +=
+                `<div class="card-part">
+            <h2>${participantes[i].nome_part}</h2>
+            <p>Nome do personagem: ${participantes[i].nome_perso}</p>
+            <p>Mídia: ${participantes[i].midia}</p>
+            <p>Prop: ${participantes[i].prop}</p>
+            <p>Modalidade desejada: ${participantes[i].modalidade}</p>
+            <p>Canonico: ${participantes[i].canon}</p>
+            
+            <button class="container-button" onclick="editarCad(${i})">Editar</button>
+            <button class="container-button" onclick="excluirCad(${i})">Excluir</button>
+            </div>
+            `
+        }
     }
-    for (let i = 0; i < participantesNegados.length; i++) {
-        document.getElementById('painel-participantes-negados').innerHTML +=
-            `<div class="card-negado">
-           <h2>${participantesNegados[i].nome_part}</h2>
-           <p>Nome do personagem: ${participantesNegados[i].nome_perso}</p>
-           <p>Mídia: ${participantesNegados[i].midia}</p>
-           <p>Prop: ${participantesNegados[i].prop}</p>
-           <p>Modalidade desejada: ${participantesNegados[i].modalidade}</p>
-           <p>Canonico: ${participantesNegados[i].canon}</p>
-        </div>
-        `
+    for (let i = 0; i < participantes.length; i++) {
+        if (participantes[i].aceito == false) {
+
+            document.getElementById('painel-participantes-negados').innerHTML +=
+                `<div class="card-negado">
+            <h2>${participantes[i].nome_part}</h2>
+            <p>Nome do personagem: ${participantes[i].nome_perso}</p>
+            <p>Mídia: ${participantes[i].midia}</p>
+            <p>Prop: ${participantes[i].prop}</p>
+            <p>Modalidade desejada: ${participantes[i].modalidade}</p>
+            <p>Canonico: ${participantes[i].canon}</p>
+            
+            <button class="container-buttonN" onclick="editarCad(${i})">Editar</button>
+            <button class="container-buttonN" onclick="excluirCad(${i})">Excluir</button>
+            </div>
+            `
+        }
     }
+
 }
 
 document.getElementById('input-nome_part').addEventListener('keyup', function (e) {
